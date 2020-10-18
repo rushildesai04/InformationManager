8b4c96062e4dec7cba006bb14601bd7319f752ab
Rushil D <66389126+rush-bot@users.noreply.github.com>
2020-10-18 12:00:28 -0700

    Delete main.py

--- Information Manager/main.py
+++
@@ -1,4 +0,0 @@
-#The main python script where everything will be connected
-#All classes, functions, methods, and executables will be excecuted here
-#When program is finished, the execution of everything will take place here
-#The most important file in the program
