2aadcbe2aeb0e26f76e0bef029c4f488b523a78d
Rushil D <66389126+rush-bot@users.noreply.github.com>
2020-10-28 12:11:35 -0700

    Add files via upload

---
+++ Information Manager/accountCreation.py
@@ -0,0 +1,16 @@
+def accountCreation():
+    from main import accountIDfile
+    print("Thank you for choosing the Information Manager")
+    print("This is a great tool for staying organized and keeping Infomation Secure")
+    print("Lets get started with creating your account\n")
+
+    print("What is your full name?")
+    name = input()
+    appendAccount =
+    accountIDfile(name)
+
+
+
+
+
+accountCreation()
--- Information Manager/main.py
+++ Information Manager/main.py
@@ -1,13 +1,23 @@
-### - File Information
-#The main python script where everything will be connected
-#All classes, functions, methods, and executables will be excecuted here
-#When program is finished, the execution of everything will take place here
-#The most important file in the program
+#Initiialization
+print("Welcome to the Information Manager.")
+print("Here you can manage all your information in a secure and organized space")
+print("If you have an account, you will need to verify yourself")
+print("If you are new, You will need to create an account\n")
 
+#Account Info
+def accountIDfile(info):
+    addInfo = open("accountID.txt", "a")
+    addInfo.write(info)
 
-### - imports
-from imports import imports
+#import pathlib
+#import os.path
+#import filecmp
+#import tempfile
+#import shutil
+#import pickle
+#import zipfile
+#import getpass
+#import sys
+#import os
 
-
-###
 
