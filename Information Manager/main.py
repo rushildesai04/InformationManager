cff5a7f9a9aa806e24943fc1b09a80e6cc0ef199
Rushil D <66389126+rush-bot@users.noreply.github.com>
2020-10-28 19:00:41 -0700

    Add files via upload

--- Information Manager/main.py
+++ Information Manager/main.py
@@ -4,10 +4,7 @@ print("Here you can manage all your information in a secure and organized space"
 print("If you have an account, you will need to verify yourself")
 print("If you are new, You will need to create an account\n")
 
-#Account Info
-def accountIDfile(info):
-    addInfo = open("accountID.txt", "a")
-    addInfo.write(info)
+
 
 #import pathlib
 #import os.path
