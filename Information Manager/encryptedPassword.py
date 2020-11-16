ba6f35a9e313c475d7b4a4b96fc1b770927a0a9b
Rushil D <66389126+rush-bot@users.noreply.github.com>
2020-11-15 23:33:10 -0800

    Delete encryptedPassword.py

--- Information Manager/encryptedPassword.py
+++
@@ -1,6 +0,0 @@
-def encryptedPassword(len):
-    import random
-    list = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    result = ''.join((random.choice(list) for i in range(len)))
-    return result
-
